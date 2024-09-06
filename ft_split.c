18a0f2017228dec8784f967d3748f6093b1b3b33
sarah <161157953+sarahgh8@users.noreply.github.com>
2024-09-06 18:02:44 +0300

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -1,97 +1,104 @@
 #include "libft.h"
-static size_t howMany_words(char const *s, char c)
-{
-    size_t num_of_words;
-    size_t j;
 
-    num_of_words = 0;
-    j = 0;
-    while(s[j] == c)
-        j++;
-    while(s[j])
-    {
-        if((!j || s[j - 1] == c) && (s[j] != c))
-            num_of_words++;
-        j++;
-    }
-    return (num_of_words);
+static size_t	how_many_words(char const *s, char c)
+{
+	size_t	num_of_words;
+	size_t	j;
+
+	num_of_words = 0;
+	j = 0;
+	while (s[j] == c)
+		j++;
+	while (s[j])
+	{
+		if ((!j || s[j - 1] == c) && (s[j] != c))
+			num_of_words++;
+		j++;
+	}
+	return (num_of_words);
 }
 
-static size_t size_of_word (char const *s, char c, size_t i)
+static size_t	size_of_word(char const *s, char c, size_t i)
 {
-    size_t size;
+	size_t	size;
 
-    size = 0;
-    while(s[i] && s[i] != c)
-    {
-        size++;
-        i++;
-    }
-    return (size);
+	size = 0;
+	while (s[i] && s[i] != c)
+	{
+		size++;
+		i++;
+	}
+	return (size);
 }
 
-void mem_free(char **res)
+char	*alloc_cpy(char *s, char c, t_intialValues *ptr_to)
 {
-    size_t i;
-    while(res[i])
-    {
-        free(res[i]);
-        i++;
-    }
-    free(res);
+	char	*res;
+	size_t	i;
+
+	i = 0;
+	ptr_to->word_size = size_of_word(s, c, ptr_to->i);
+	res = malloc((ptr_to->word_size + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (s[ptr_to->i] && s[ptr_to->i] != c)
+		res[i++] = s[ptr_to->i++];
+	res[i] = '\0';
+	return (res);
 }
 
-char    **ft_split(char const *s, char c)
+static void	mem_free(char **res, size_t j)
 {
-    char **res;
-    size_t i;
-    size_t j;
-    size_t k;
-    size_t word_size;
+	size_t	i;
 
-    i = 0;
-    j = 0;
-    res = malloc((howMany_words(s, c) + 1) * sizeof(char *));
-    
-    if(!res)
-    {
-        return (NULL);
-        mem_free(res);
-    }
-    while(s[i])
-    {
-        if(s[i] != c)
-        {
-            word_size = size_of_word(s, c , i);
-            res[j] = malloc((word_size + 1) * sizeof(char));
-            if(!res[j])
-            {
-                mem_free(res);
-                return (NULL);
-            }
-            k = 0;
-            while(s[i] && s[i] != c)
-                res[j][k++] = s[i++];
-            res[j][k] = '\0';
-            j++;
-        }
-        else
-            i++;
-    }
-    res[j] = NULL;
-    return (res);
+	i = 0;
+	while (i < j)
+	{
+		free(res[i]);
+		i++;
+	}
+	free(res);
 }
-int main ()
+
+char	**ft_split(char const *s, char c)
 {
-    char **res;
-    size_t i;
+	char			**res;
+	t_intialValues	ptr_to;
 
-    res = ft_split("hello!", ' ');
-    i = 0;
-    while(res[i])
-    {
-        printf("%s\n", res[i]);
-        i++;
-    }
-    return (0);
+	ptr_to.i = 0;
+	ptr_to.j = 0;
+	res = malloc((how_many_words(s, c) + 1) * sizeof(char *));
+	if (!res)
+		return (NULL);
+	while (s[ptr_to.i])
+	{
+		if (s[ptr_to.i] != c)
+		{
+			res[ptr_to.j] = alloc_cpy((char *)s, c, &ptr_to);
+			if (!res[ptr_to.j])
+			{
+				mem_free(res, ptr_to.j);
+				return (NULL);
+			}
+			ptr_to.j++;
+		}
+		else
+			ptr_to.i++;
+	}
+	res[ptr_to.j] = NULL;
+	return (res);
 }
+// int main ()
+// {
+//     char **res;
+//     size_t i;
+
+//     res = ft_split("hello!", ' ');
+//     i = 0;
+//     while(res[i])
+//     {
+//         printf("%s\n", res[i]);
+//         i++;
+//     }
+//     return (0);
+// }
